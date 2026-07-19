42c1d9d113daed213d28a6e818ccd8e0a826c734
Nazifa Anjum Turna <nazifaanjumturna@gmail.com>
2026-07-20 00:43:57 +0600

    First python practice

---
+++ math_function.py
@@ -0,0 +1,19 @@
+import math 
+
+#ceil is a mathematical function that returns the smallest integer greater than or equal to a given number.
+x=4.3
+y=0.004
+print(math.ceil(x)) 
+print(math.ceil(y))
+
+#floor is the opposite of ceil. It returns the largest integer less than or equal to a given number.
+x=4.3
+y=0.004
+print(math.floor(x))
+print(math.floor(y))
+
+#round is a built-in function in Python that rounds a number to the nearest integer. If the fractional part of the number is 0.5 or greater, it rounds up to the next integer. If the fractional part is less than 0.5, it rounds down to the previous integer.
+x=4.5
+y=4.51
+print(round(x)) 
+print(round(y))
