8562fdcbb6ab97eaf394ca35b25acba89a8c0a36
Nazifa Anjum Turna <nazifaanjumturna@gmail.com>
2026-07-19 22:06:23 +0600

    Update operators.py

--- operators.py
+++ operators.py
@@ -6,7 +6,6 @@ e=4%5 # Modulus
 f=4**5 #4 to the power of 5
 g=4//5 # Floor Division
 
-
 print("Addition of 4 and 5 is:",a)
 print("Subtraction of 4 and 5 is:",b)
 print("Multiplication of 4 and 5 is:",c)
@@ -14,10 +13,8 @@ print("Division of 4 and 5 is:",d)
 print("Modulus of 4 and 5 is:",e)
 print("Exponentiation of 4 and 5 is:",f)
 print("Floor Division of 4 and 5 is:",g)
-
-print(a, b, c, int(d), e, f)
-
-print(a, b, c, d, e, f)
+#print(a, b, c, int(d), e, f)
+#print(a, b, c, d, e, f)
 
 #BODMAS Rule or PEMDAS Rule
 
