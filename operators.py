ab8f251dfb4affecf03ce456f3155294ad85fbaa
Nazifa Anjum Turna <nazifaanjumturna@gmail.com>
2026-07-19 21:46:51 +0600

    First Python Practice

---
+++ operators.py
@@ -0,0 +1,20 @@
+a=4+5
+b=4-5
+c=4*5
+d=4/5
+e=4%5
+f=4**5
+g=4//5
+
+
+print("Addition of 4 and 5 is:",a)
+print("Subtraction of 4 and 5 is:",b)
+print("Multiplication of 4 and 5 is:",c)
+print("Division of 4 and 5 is:",d)
+print("Modulus of 4 and 5 is:",e)
+print("Exponentiation of 4 and 5 is:",f)
+print("Floor Division of 4 and 5 is:",g)
+
+# print(a, b, c, int(d), e, f)
+
+# print(a, b, c, d, e, f)
