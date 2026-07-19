4eefdf0397ca493773b919ab2d69f72350053ee0
Nazifa Anjum Turna <nazifaanjumturna@gmail.com>
2026-07-19 22:06:00 +0600

    First python practice

--- operators.py
+++ operators.py
@@ -1,10 +1,10 @@
-a=4+5
-b=4-5
-c=4*5
-d=4/5
-e=4%5
-f=4**5
-g=4//5
+a=4+5 # Addition
+b=4-5 # Subtraction
+c=4*5 # Multiplication
+d=4/5 # Division
+e=4%5 # Modulus
+f=4**5 #4 to the power of 5
+g=4//5 # Floor Division
 
 
 print("Addition of 4 and 5 is:",a)
@@ -15,6 +15,11 @@ print("Modulus of 4 and 5 is:",e)
 print("Exponentiation of 4 and 5 is:",f)
 print("Floor Division of 4 and 5 is:",g)
 
-# print(a, b, c, int(d), e, f)
+print(a, b, c, int(d), e, f)
 
-# print(a, b, c, d, e, f)
+print(a, b, c, d, e, f)
+
+#BODMAS Rule or PEMDAS Rule
+
+x=(4+5)-5+(4*5)/4+(4**5)
+print("The value of x is:",x)
