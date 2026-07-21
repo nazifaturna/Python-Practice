7008bae5ff34064699af1746557d1698433cb597
Nazifa Anjum Turna <nazifaanjumturna@gmail.com>
2026-07-21 19:38:16 +0600

    First Python Practice

---
+++ conditional_statement.py
@@ -0,0 +1,31 @@
+print(10==4)
+print(10==10)
+print( )
+a = 50
+if a>4:
+    print("a is greater than 4")
+else:
+    print("a is not greater than 4")  
+if a>10:   
+    print("a is greater than 10")
+else:
+    print("a is not greater than 10")
+print( )
+
+a=int(input("Enter your marks: "))
+if a>=80:
+    print("Grade A+")
+elif a>=75:
+    print("Grade A")
+elif a>=70:
+    print("Grade B+")
+elif a>=65:
+    print("Grade B")
+elif a>=60:
+    print("Grade C+")
+elif a>=50:
+    print("Grade C")
+elif a>=40:
+    print("Grade D")
+else:
+    print("Grade F")
